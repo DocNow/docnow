50b9f279b1510562eb3a4db8ce8095c6985b0d2f
Ed Summers <ehs@pobox.com>
2017-03-03 16:00:10 -0500

    changes for webpack2

--- docnow/api.py
+++ docnow/api.py
@@ -1,10 +1,17 @@
+import datetime
+
 from docnow.models import Trend
 from django.db.models import Max
 from django.http import JsonResponse
 
 def trends(request):
-    latest = Trend.objects.latest('created').created
-    trends = Trend.objects.filter(created=latest).order_by('-tweets')
+    try:
+        latest = Trend.objects.latest('created').created
+        trends = Trend.objects.filter(created=latest).order_by('-tweets')
+    except Trend.DoesNotExist:
+        latest = datetime.datetime.now()
+        trends = []
+
     data = {
         "created": latest,
         "trends": [{"text": t.text, "tweets": t.tweets} for t in trends]
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,70 +3,89 @@ const path = require('path');
 var BundleTracker = require('webpack-bundle-tracker');
 
 const options = {
-  debug: true,
-
   devtool: 'inline-source-map',
 
   entry: [
     './assets/js/index.js'
   ],
+
   output: {
     //path: path.resolve('./assets/bundles/'),
     filename: "[name]-[hash].js",
     publicPath: 'http://0.0.0.0:3000/assets/bundles/', // Tell django to use this URL to load packages and not use STATIC_URL + bundle_name
   },
+
   devtool: 'cheap-module-eval-source-map',
+
   devServer: {
     host: "0.0.0.0",
     port: 3000,
-    colors: true,
     quiet: true,
     hot: true,
     inline: true,
     headers: { 'Access-Control-Allow-Origin': '*' }
   },
+
   resolve: {
-    modulesDirectories: ['node_modules', 'vendor'],
-    extensions: ['', '.js', '.json', '.css']
+    modules: ['node_modules', 'vendor'],
+    extensions: ['.js', '.json', '.css']
   },
+
   module: {
-    preLoaders: [
-      {
-        test: /\.js$/,
-        loader: 'eslint',
-        include: ['src'],
-      }
-    ],
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          babelrc: false,
-          presets: ['babel-preset-es2015', 'babel-preset-react']
-        }
+        enforce: 'pre',
+        use: [
+          {
+            loader: 'eslint-loader'
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['babel-preset-es2015', {modules: false}],
+                ['babel-preset-react']
+              ]
+            }
+          }
+        ]
       },
       {
         test: /\.css$/,
-        loader: 'style!css'
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
       },
-      {
+      { 
         test: /\.(png|jpg|svg)$/,
-        loader: "file-loader?name=images/[name].[ext]"
+        use: [
+          {
+            loader: "file-loader?name=images/[name].[ext]"
+          }
+        ]
       }
     ]
   },
-  eslint: {
-    configFile: path.join(__dirname, 'eslint.js'),
-    useEslintrc: false
-  },
+
   plugins: [
     new BundleTracker({filename: './webpack-stats.json'}),
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"development"' }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.ProvidePlugin({
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+      'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
     })
   ]
 };
