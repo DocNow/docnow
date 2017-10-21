344e5dcb5c5a8faf304dca5f462f7c6f38eca266
Ed Summers <ehs@pobox.com>
2017-10-21 17:36:55 -0400

    forgot these for #39

---
+++ src/client/components/VideoList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import style from '../styles/Videos.css'
+
+export default class VideoList extends Component {
+
+  render() {
+    let loader = null
+    if (this.props.videos.length === 0) {
+      loader = 'Loading...'
+    }
+    return (
+      <div className={style.VideosCard}>
+        {loader}
+        {this.props.videos.map((video) => (
+          <div className={style.Video}>
+            <video controls src={video.url} />
+            <div>
+              {video.count} shares
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
+}
+
+VideoList.propTypes = {
+  videos: PropTypes.array
+}
