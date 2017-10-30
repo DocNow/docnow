be84b13659eb6ad1c4ae9f239fe06d53a7c80c8b
Ed Summers <ehs@pobox.com>
2017-10-30 12:58:25 -0400

    render svg

--- src/client/components/HashtagList.js
+++ src/client/components/HashtagList.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Hashtag from './Hashtag'
+// import Hashtag from './Hashtag'
 import styles from '../styles/Hashtags.css'
+import { scaleLinear, max } from 'd3'
 
 export default class Hashtags extends Component {
 
   render() {
+
+    const maxX = max(this.props.hashtags.map((ht) => {return ht.count}))
+    const scaleX = scaleLinear()
+          .domain([0, maxX])
+          .range([0, 300])
+
     let loader = null
     if (this.props.hashtags.length === 0) {
       loader = 'Loading...'
     }
+
     return (
         <div className={styles.HashtagsCard}>
           {loader}
-          {this.props.hashtags.map(ht => (
-            <Hashtag key={ht.hashtag} data={ht}/>
+          <svg height={800}>
+          {this.props.hashtags.map((ht, i) => (
+            <rect
+              className={styles.HashtagBar}
+              x={0}
+              y={i * 25}
+              key={'hashtag-' + i}
+              height={20}
+              width={scaleX(ht.count)} />
           ))}
+          </svg>
         </div>
     )
   }
