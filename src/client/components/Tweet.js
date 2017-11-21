59d4fc31a05ebf0a7c79a63d186f8121d923e4b7
Alexandra DM <alexandra.dolanmescal@gmail.com>
2017-11-20 21:36:03 -0500

    change tweet look
    add padding remove hr

--- src/client/components/Tweet.js
+++ src/client/components/Tweet.js
@@ -19,7 +19,6 @@ export default class Tweet extends Component {
           <hr />
           <a href={this.props.data.twitterUrl}>{created}</a>
         </div>
-        <hr />
         <p>{this.props.data.text}</p>
         <div className={style.Inline}><i className="fa fa-comment-o" />{this.props.data.replyCount}</div>
         <div className={style.Inline}><i className="fa fa-retweet" />{this.props.data.retweetCount}</div>
