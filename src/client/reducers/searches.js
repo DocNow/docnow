9e0c72e134372091d200909c3b51c15aedfd6d6e
Ed Summers <ehs@pobox.com>
2018-02-12 04:55:13 -0500

    guard against stale updates to the list of searches

--- src/client/reducers/searches.js
+++ src/client/reducers/searches.js
@@ -7,7 +7,34 @@ export default function settings(state = initialState, action) {
   switch (action.type) {
 
     case SET_SEARCHES: {
-      return action.searches
+
+      /*
+       * Since the state of a search can be manipulated in the UI while an
+       * AJAX call to update the searches is going on we need to be careful
+       * not to update the searches with information that is stale.
+       *
+       * To do this we compare the updated times of the searches we've been
+       * given with the updated times of the searches we currently have.
+       * If any of the searches we have are newer than the searches we've
+       * been given then we declare the information stale and return
+       * the current state.
+       */
+
+      const updateTimes = new Map()
+      state.map(s => updateTimes.set(s.id, s.updated))
+
+      let stale = false
+      for (const s of action.searches) {
+        if (s.updated < updateTimes.get(s.id)) {
+          stale = true
+        }
+      }
+
+      if (stale) {
+        return state
+      } else {
+        return action.searches
+      }
     }
 
     case SET_TWITTER_SEARCH: {
