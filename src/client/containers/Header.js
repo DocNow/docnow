486aa77d803fa50e404a12be69674a8557e00ce8
Ed Summers <ehs@pobox.com>
2018-03-08 21:29:24 -0500

    zero for now

--- src/client/containers/Header.js
+++ src/client/containers/Header.js
@@ -10,7 +10,7 @@ const mapStateToProps = (state) => {
     isSuperUser: state.user.isSuperUser,
     location: state.router.location.pathname,
     logoUrl: state.settings.logoUrl,
-    notifications: 2
+    notifications: 0 // todo: map to state
   }
 }
 
