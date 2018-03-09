164c3476807f1e197371f59d6a61874ae507a9e8
Ed Summers <ehs@pobox.com>
2018-03-08 21:24:39 -0500

    latest build

--- dist/server/api.js
+++ dist/server/api.js
@@ -363,44 +363,6 @@ app.get('/search/:searchId/tweets', function (req, res) {
   }
 });
 
-app.put('/search/:searchId', function (req, res) {
-  if (req.user) {
-    db.getSearch(req.body.id).then(function () {
-      var _ref4 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee4(search) {
-        return _regenerator2.default.wrap(function _callee4$(_context4) {
-          while (1) {
-            switch (_context4.prev = _context4.next) {
-              case 0:
-                _context4.next = 2;
-                return db.updateSearch(search);
-
-              case 2:
-                if (req.query.refreshTweets) {
-                  db.importFromSearch(search).then(function () {
-                    res.json(search);
-                  }).catch(function (e) {
-                    _logger2.default.error('search failed', e);
-                    res.json(search);
-                  });
-                } else {
-                  res.json(search);
-                }
-
-              case 3:
-              case 'end':
-                return _context4.stop();
-            }
-          }
-        }, _callee4, undefined);
-      }));
-
-      return function (_x7) {
-        return _ref4.apply(this, arguments);
-      };
-    }());
-  }
-});
-
 app.get('/search/:searchId/users', function (req, res) {
   if (req.user) {
     db.getSearch(req.params.searchId).then(function (search) {
@@ -452,14 +414,52 @@ app.get('/search/:searchId/videos', function (req, res) {
 });
 
 app.get('/search/:searchId/webpages', function () {
-  var _ref5 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee5(req, res) {
+  var _ref4 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee4(req, res) {
     var search, webpages;
+    return _regenerator2.default.wrap(function _callee4$(_context4) {
+      while (1) {
+        switch (_context4.prev = _context4.next) {
+          case 0:
+            if (!req.user) {
+              _context4.next = 8;
+              break;
+            }
+
+            _context4.next = 3;
+            return db.getSearch(req.params.searchId);
+
+          case 3:
+            search = _context4.sent;
+            _context4.next = 6;
+            return db.getWebpages(search);
+
+          case 6:
+            webpages = _context4.sent;
+
+            res.json(webpages);
+
+          case 8:
+          case 'end':
+            return _context4.stop();
+        }
+      }
+    }, _callee4, undefined);
+  }));
+
+  return function (_x7, _x8) {
+    return _ref4.apply(this, arguments);
+  };
+}());
+
+app.put('/search/:searchId/webpages', function () {
+  var _ref5 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee5(req, res) {
+    var search, url;
     return _regenerator2.default.wrap(function _callee5$(_context5) {
       while (1) {
         switch (_context5.prev = _context5.next) {
           case 0:
             if (!req.user) {
-              _context5.next = 8;
+              _context5.next = 14;
               break;
             }
 
@@ -468,15 +468,33 @@ app.get('/search/:searchId/webpages', function () {
 
           case 3:
             search = _context5.sent;
-            _context5.next = 6;
-            return db.getWebpages(search);
+            url = req.body.url;
 
-          case 6:
-            webpages = _context5.sent;
+            if (!(req.body.selected === true)) {
+              _context5.next = 10;
+              break;
+            }
 
-            res.json(webpages);
+            _context5.next = 8;
+            return db.selectWebpage(search, url);
 
           case 8:
+            _context5.next = 13;
+            break;
+
+          case 10:
+            if (!(req.body.deselected === true)) {
+              _context5.next = 13;
+              break;
+            }
+
+            _context5.next = 13;
+            return db.deselectWebpage(search, url);
+
+          case 13:
+            res.json({ status: 'updated' });
+
+          case 14:
           case 'end':
             return _context5.stop();
         }
@@ -484,20 +502,20 @@ app.get('/search/:searchId/webpages', function () {
     }, _callee5, undefined);
   }));
 
-  return function (_x8, _x9) {
+  return function (_x9, _x10) {
     return _ref5.apply(this, arguments);
   };
 }());
 
-app.put('/search/:searchId/webpages', function () {
+app.get('/search/:searchId/queue', function () {
   var _ref6 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee6(req, res) {
-    var search, url;
+    var search, result;
     return _regenerator2.default.wrap(function _callee6$(_context6) {
       while (1) {
         switch (_context6.prev = _context6.next) {
           case 0:
             if (!req.user) {
-              _context6.next = 14;
+              _context6.next = 8;
               break;
             }
 
@@ -506,33 +524,15 @@ app.put('/search/:searchId/webpages', function () {
 
           case 3:
             search = _context6.sent;
-            url = req.body.url;
+            _context6.next = 6;
+            return db.queueStats(search);
 
-            if (!(req.body.selected === true)) {
-              _context6.next = 10;
-              break;
-            }
+          case 6:
+            result = _context6.sent;
 
-            _context6.next = 8;
-            return db.selectWebpage(search, url);
+            res.json(result);
 
           case 8:
-            _context6.next = 13;
-            break;
-
-          case 10:
-            if (!(req.body.deselected === true)) {
-              _context6.next = 13;
-              break;
-            }
-
-            _context6.next = 13;
-            return db.deselectWebpage(search, url);
-
-          case 13:
-            res.json({ status: 'updated' });
-
-          case 14:
           case 'end':
             return _context6.stop();
         }
@@ -540,37 +540,32 @@ app.put('/search/:searchId/webpages', function () {
     }, _callee6, undefined);
   }));
 
-  return function (_x10, _x11) {
+  return function (_x11, _x12) {
     return _ref6.apply(this, arguments);
   };
 }());
 
-app.get('/search/:searchId/queue', function () {
+app.get('/wayback/:url', function () {
   var _ref7 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee7(req, res) {
-    var search, result;
+    var result;
     return _regenerator2.default.wrap(function _callee7$(_context7) {
       while (1) {
         switch (_context7.prev = _context7.next) {
           case 0:
             if (!req.user) {
-              _context7.next = 8;
+              _context7.next = 5;
               break;
             }
 
             _context7.next = 3;
-            return db.getSearch(req.params.searchId);
+            return _wayback2.default.closest(req.params.url);
 
           case 3:
-            search = _context7.sent;
-            _context7.next = 6;
-            return db.queueStats(search);
-
-          case 6:
             result = _context7.sent;
 
             res.json(result);
 
-          case 8:
+          case 5:
           case 'end':
             return _context7.stop();
         }
@@ -578,12 +573,12 @@ app.get('/search/:searchId/queue', function () {
     }, _callee7, undefined);
   }));
 
-  return function (_x12, _x13) {
+  return function (_x13, _x14) {
     return _ref7.apply(this, arguments);
   };
 }());
 
-app.get('/wayback/:url', function () {
+app.put('/wayback/:url', function () {
   var _ref8 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee8(req, res) {
     var result;
     return _regenerator2.default.wrap(function _callee8$(_context8) {
@@ -596,7 +591,7 @@ app.get('/wayback/:url', function () {
             }
 
             _context8.next = 3;
-            return _wayback2.default.closest(req.params.url);
+            return _wayback2.default.saveArchive(req.params.url);
 
           case 3:
             result = _context8.sent;
@@ -611,41 +606,8 @@ app.get('/wayback/:url', function () {
     }, _callee8, undefined);
   }));
 
-  return function (_x14, _x15) {
+  return function (_x15, _x16) {
     return _ref8.apply(this, arguments);
-  };
-}());
-
-app.put('/wayback/:url', function () {
-  var _ref9 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee9(req, res) {
-    var result;
-    return _regenerator2.default.wrap(function _callee9$(_context9) {
-      while (1) {
-        switch (_context9.prev = _context9.next) {
-          case 0:
-            if (!req.user) {
-              _context9.next = 5;
-              break;
-            }
-
-            _context9.next = 3;
-            return _wayback2.default.saveArchive(req.params.url);
-
-          case 3:
-            result = _context9.sent;
-
-            res.json(result);
-
-          case 5:
-          case 'end':
-            return _context9.stop();
-        }
-      }
-    }, _callee9, undefined);
-  }));
-
-  return function (_x16, _x17) {
-    return _ref9.apply(this, arguments);
   };
 }());
 
