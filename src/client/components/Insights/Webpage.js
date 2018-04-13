a5fbd1f24af93c3b929de0cd9bb323df4d44d93d
Alexandra DM <alexandra.dolanmescal@gmail.com>
2018-04-13 09:52:55 -0400

    start webpage CSS updates
    change icons and icon colors.

--- src/client/components/Insights/Webpage.js
+++ src/client/components/Insights/Webpage.js
@@ -42,9 +42,9 @@ export default class Webpage extends Component {
       <div className={card.Card + ' ' +  selectedStyle}>
         <div className={style.Image}>
           <div className={style.Controls}>
-            <i onClick={() => {this.select()}} className={style.Add + ' fa fa-thumbs-up'} />
+            <i onClick={() => {this.select()}} className={style.Add + ' fa fa-check-circle"'} />
             &nbsp;
-            <i onClick={() => {this.deselect()}} className={style.Remove + ' fa fa-thumbs-down'}/>
+            <i onClick={() => {this.deselect()}} className={style.Remove + ' fa fa-eye-slash'}/>
           </div>
           <a href={this.props.url} target="_new">
             <img rel="noreferrer" src={img} />
