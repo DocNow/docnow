968c79d1a9fb4e5d1df51a2961ed9b643d96d366
raffazizzi <raffaeleviglianti@gmail.com>
2019-08-12 16:23:28 -0400

    Fixed autocomplete list scroll issue

--- src/client/components/Trends/AddPlace.js
+++ src/client/components/Trends/AddPlace.js
@@ -54,6 +54,15 @@ export default class AddPlace extends Component {
           sortItems={this.sortPlaces}
           items={Object.keys(this.props.world)}
           shouldItemRender={this.matchInputToTerm}
+          menuStyle={{
+            borderRadius: '3px',
+            boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
+            background: 'rgba(255, 255, 255, 0.9)',
+            padding: '2px 0',
+            fontSize: '90%',
+            overflowY: 'scroll',
+            maxHeight: '250px'
+          }}
           renderItem={(item, isHighlighted) => (
             <div key={item} style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
               {item}
