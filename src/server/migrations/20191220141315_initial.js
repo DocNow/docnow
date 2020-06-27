1ed1966906ea48cbb4b1dac414666eefcd92e29d
Ed Summers <ehs@pobox.com>
2020-06-27 12:48:47 -0400

    drop constraints before dropping tables

--- src/server/migrations/20191220141315_initial.js
+++ src/server/migrations/20191220141315_initial.js
@@ -6,19 +6,22 @@ exports.up = knex => {
       table.increments('id').primary()
       table.string('name').unique()
       table.string('value')
-      table.timestamp('created_at')
+      table.timestamp('created')
     })
 
     .createTable('user', table => {
       table.increments('id').primary()
       table.datetime('created').defaultsTo(knex.fn.now())
       table.datetime('updated').notNullable()
+      table.string('name')
       table.string('email')
+      table.string('location')
       table.boolean('admin').defaultsTo(false)
-      table.integer('tweet_quota').notNullable()
-      table.string('twitter_screen_name').notNullable()
-      table.string('twitter_consumer_key').notNullable()
-      table.string('twitter_consumer_secret').notNullable()
+      table.integer('tweetQuota').notNullable()
+      table.string('twitterScreenName').notNullable()
+      table.string('twitterConsumerKey').notNullable()
+      table.string('twitterConsumerSecret').notNullable()
+      table.boolean('isSuperUser').defaultsTo(false)
     })
 
     .createTable('place', table => {
@@ -33,57 +36,57 @@ exports.up = knex => {
     .createTable('trend', table => {
       table.increments('id').primary()
       table.integer('count').notNullable()
-      table.integer('place_id').notNullable()
-      table.foreign('place_id').references('id').inTable('place')
+      table.integer('placeId').notNullable()
+      table.foreign('placeId').references('id').inTable('place')
     })
 
-    .createTable('user_place', table => {
+    .createTable('userPlace', table => {
       table.increments('id').primary()
       table.integer('position').notNullable()
-      table.integer('user_id').notNullable()
-      table.integer('place_id').notNullable()
-      table.foreign('user_id').references('id').inTable('user')
-      table.foreign('place_id').references('id').inTable('place')
+      table.integer('userId').notNullable()
+      table.integer('placeId').notNullable()
+      table.foreign('userId').references('id').inTable('user')
+      table.foreign('placeId').references('id').inTable('place')
     })
 
     .createTable('collection', table => {
       table.increments('id').primary()
       table.datetime('created').defaultsTo(knex.fn.now())
       table.datetime('updated').notNullable()
-      table.integer('user_id').notNullable()
+      table.integer('userId').notNullable()
       table.string('title').notNullable()
       table.text('description')
       table.boolean('active').defaultTo(false)
-      table.foreign('user_id').references('id').inTable('user')
+      table.foreign('userId').references('id').inTable('user')
     })
 
     .createTable('query', table => {
       table.increments('id').primary()
-      table.integer('collection_id').notNullable()
+      table.integer('collectionId').notNullable()
       table.datetime('created').defaultsTo(knex.fn.now())
       table.text('text').notNullable()
-      table.foreign('collection_id').references('id').inTable('collection')
+      table.foreign('collectionId').references('id').inTable('collection')
     })
 
     .createTable('tweet', table => {
       table.increments('id').primary()
-      table.string('tweet_id', 30).notNullable()
-      table.integer('collection_id').notNullable()
-      table.string('screen_name').notNullable()
+      table.string('tweetId', 30).notNullable()
+      table.integer('collectionId').notNullable()
+      table.string('screenName').notNullable()
       table.text('text').notNullable()
-      table.string('retweet_id', 30)
-      table.string('quote_id', 30)
-      table.integer('retweet_count').defaultsTo(0)
-      table.integer('reply_count').defaultsTo(0)
-      table.integer('quote_count').defaultsTo(0)
-      table.integer('like_count').defaultsTo(0)
-      table.string('reply_to_tweet_id', 30)
-      table.string('reply_to_user_id', 30)
-      table.integer('image_count').defaultsTo(0)
-      table.integer('video_count').defaultsTo(0)
+      table.string('retweetId', 30)
+      table.string('quoteId', 30)
+      table.integer('retweetCount').defaultsTo(0)
+      table.integer('replyCount').defaultsTo(0)
+      table.integer('quoteCount').defaultsTo(0)
+      table.integer('likeCount').defaultsTo(0)
+      table.string('replyToTweetId', 30)
+      table.string('replyToUserId', 30)
+      table.integer('imageCount').defaultsTo(0)
+      table.integer('videoCount').defaultsTo(0)
       table.string('language', 10).notNullable()
       table.jsonb('json').notNullable()
-      table.foreign('collection_id').references('id').inTable('collection')
+      table.foreign('collectionId').references('id').inTable('collection')
     })
 
     .createTable('webpage', table => {
@@ -95,12 +98,12 @@ exports.up = knex => {
       table.text('image')
     })
 
-    .createTable('tweet_webpage', table => {
-      table.integer('tweet_id').notNullable()
-      table.integer('webpage_id').notNullable()
-      table.foreign('tweet_id').references('id').inTable('tweet')
-      table.foreign('webpage_id').references('id').inTable('webpage')
-      table.primary(['tweet_id', 'webpage_id'])
+    .createTable('tweetWebpage', table => {
+      table.integer('tweetId').notNullable()
+      table.integer('webpageId').notNullable()
+      table.foreign('tweetId').references('id').inTable('tweet')
+      table.foreign('webpageId').references('id').inTable('webpage')
+      table.primary(['tweetId', 'webpageId'])
     })
 
     .createTable('hashtag', table => {
@@ -108,24 +111,54 @@ exports.up = knex => {
       table.datetime('created').defaultsTo(knex.fn.now())
     })
 
-    .createTable('tweet_hashtag', table => {
-      table.integer('tweet_id').notNullable()
-      table.string('hashtag_name', 255).notNullable()
-      table.foreign('tweet_id').references('id').inTable('tweet')
-      table.foreign('hashtag_name').references('name').inTable('hashtag')
-      table.primary(['tweet_id', 'hashtag_name'])
+    .createTable('tweetHashtag', table => {
+      table.integer('tweetId').notNullable()
+      table.string('hashtagName', 255).notNullable()
+      table.foreign('tweetId').references('id').inTable('tweet')
+      table.foreign('hashtagName').references('name').inTable('hashtag')
+      table.primary(['tweetId', 'hashtagName'])
     })
 }
 
-exports.down = (knex) => {
-  return knex.schema 
+exports.down = async (knex) => {
+
+  // need to disable all the foreign keys before deleting the tables
+
+  return knex.schema
+    .table('user_place', table => {
+      table.dropForeign(null, 'userplace_placeid_foreign')
+      table.dropForeign(null, 'userplace_userid_foreign')
+    })
+    .table('trend', table => {
+      table.dropForeign(null, 'trend_placeid_foreign')
+    })
+    .table('query', table => {
+      table.dropForeign(null, 'query_collectionid_foreign')
+    })
+    .table('collection', table => {
+      table.dropForeign(null, 'collection_userid_foreign')
+    })
+    .table('tweet', table => {
+      table.dropForeign(null, 'tweet_collectionid_foreign')
+    })
+    .table('tweet_webpage', table => {
+      table.dropForeign(null, 'tweetwebpage_tweetid_foreign')
+      table.dropForeign(null, 'tweetwebpage_webpageid_foreign')
+    })
+    .table('tweet_hashtag', table => {
+      table.dropForeign(null, 'tweethashtag_tweetid_foreign')
+      table.dropForeign(null, 'tweethashtag_hashtagname_foreign')
+    })
     .dropTable('setting')
-    .dropTable('user')
+    .dropTable('userPlace')
     .dropTable('place')
+    .dropTable('user')
     .dropTable('trend')
     .dropTable('collection')
     .dropTable('query')
     .dropTable('tweet')
     .dropTable('webpage')
     .dropTable('hashtag')
+    .dropTable('tweetHashtag')
+    .dropTable('tweetWebpage')
 }
