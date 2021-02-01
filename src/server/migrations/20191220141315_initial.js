011d3a2a9f022d8cc2b3a1b6dddd218f39ee7a1e
Ed Summers <ehs@pobox.com>
2021-02-01 17:01:53 -0500

    Fixed exception related to large Twitter user id lookup.

--- src/server/migrations/20191220141315_initial.js
+++ src/server/migrations/20191220141315_initial.js
@@ -21,7 +21,7 @@ exports.up = knex => {
       table.string('location')
       table.string('email')
       table.string('description')
-      table.integer('twitterUserId').notNullable()
+      table.string('twitterUserId', 30).notNullable()
       table.string('twitterScreenName').notNullable()
       table.string('twitterAccessToken').notNullable()
       table.string('twitterAccessTokenSecret').notNullable()
