7b9d42dbaa98fc7a3ef4dbfef7e3d58f0289ddbf
Ed Summers <ehs@pobox.com>
2021-02-01 22:11:00 -0500

    moving from travis to github actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ $NODE_ENV = "development" ]
-then
+if [ $NODE_ENV = "development" ]; then
   npm run migrate
   npm run start:dev
+elif [ $NODE_ENV = "test" ]; then
+  npm run migrate
+  npm run test
 else
   npm run start
 fi
