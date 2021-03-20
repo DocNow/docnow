1db07f15122bb5277ffca0ed2f72c7e7ccb408ce
Ed Summers <ehs@pobox.com>
2021-03-20 13:23:48 -0400

    no limit to saved

--- src/server/db.js
+++ src/server/db.js
@@ -177,7 +177,6 @@ export class Database {
     const users = await User.query()
       .withGraphJoined('places')
       .withGraphJoined('searches')
-      .where('searches.saved', true)
 
     // this is a stop gap until redis goes away
     // we need to add aggregate stats to each search
