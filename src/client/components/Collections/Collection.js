c88cd144b0d3ee174ed9758db29b9f9131ab66a8
raffazizzi <raffaeleviglianti@gmail.com>
2021-03-23 17:43:36 -0400

    Some work on my tweets card

--- src/client/components/Collections/Collection.js
+++ src/client/components/Collections/Collection.js
@@ -9,6 +9,11 @@ import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import FormControl from '@material-ui/core/FormControl'
+import Button from '@material-ui/core/Button'
 
 import listStyle from './CollectionList.css'
 import style from './Collection.css'
@@ -34,8 +39,10 @@ export default class CollectionList extends Component {
   render() {
     // don't render until we at least know the title of the collection
     if (!this.props.search.title) {
-      return <div />
+      return <div/>
     }
+
+    const tweetCount = this.props.search.tweetCount.toLocaleString()
 
     const contact = this.props.search.creator.email
       ? <a href={`mailto:${this.props.search.creator.email}`}>{this.props.search.creator.email}</a>
@@ -47,13 +54,45 @@ export default class CollectionList extends Component {
       })
     }
 
+    let userTweets = (<div className={style.CardInnerContent}>
+      <Typography variant="body1">Authenticate to Twitter to find out if any of your tweets are
+      in this collection.</Typography>
+      <Typography variant="body1">When you authenticate, you can specify consent, request removal,
+      or start a conversation with the collector about your content.</Typography>
+    </div>)
+
+    if (this.props.user) {
+      const foundTweetsCount = this.props.user.foundInSearches[this.props.searchId]
+      if (foundTweetsCount > 0) {
+        userTweets = (<>
+          <FormControl component="fieldset" className={style.CardInnerContent}>
+            <FormGroup row>
+              <FormControlLabel
+                value="all"
+                control={<Checkbox color="primary" />}
+                label={`Select all ${foundTweetsCount} tweets`}
+              />
+            </FormGroup>
+          </FormControl>
+          <Grid container spacing={0}>
+            <Grid item xs={4}><Button className={style.Options}>specify consent</Button></Grid>
+            <Grid item xs={4}><Button className={style.Options}>request removal</Button></Grid>
+            <Grid item xs={4}><Button className={style.Options}>talk with us</Button></Grid>
+          </Grid>
+          <hr/>
+        </>)
+      } else {
+        userTweets = <Typography variant="body1">There are no tweets by you in this collection.</Typography>
+      }
+    }
+
     return (
       <>
         <Grid container spacing={3} className={listStyle.Header}>
           <Grid item xs={12} className={listStyle.Title}>
             <Typography variant="h2">
-              <Link to="/collections">ALL COLLECTIONS</Link> ➔ {this.props.search.title}
-            </Typography>
+              <Link to="/collections">ALL COLLECTIONS</Link> ➔ {this.props.search.title.toUpperCase()}
+            </Typography>            
           </Grid>
           <Grid item xs={12}>
             <Typography variant="body1">
@@ -77,8 +116,9 @@ export default class CollectionList extends Component {
           <Card raised className={card.Card} >
             <CardContent>
               <div className={style.CardTitle}>
-                <FindMe user={this.props.user} />
+                <FindMe user={this.props.user} dest={`/collection/${this.props.searchId}`}/>                
               </div>
+              {userTweets}
             </CardContent>
           </Card>
           <Card raised className={card.Card} >
@@ -91,7 +131,7 @@ export default class CollectionList extends Component {
           <Card raised className={card.Card} >
             <CardContent className={card.Scroll}>
               <Typography variant="h2" className={style.CardTitle}>
-                {this.props.search.tweetCount.toLocaleString()} tweets (all users)
+                {tweetCount} tweets (all users)
               </Typography>
               {tweets}
             </CardContent>
