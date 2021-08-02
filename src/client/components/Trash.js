4b560e1bafdb32f40ffa3605bc70179d726c8f9b
Ed Summers <ehs@pobox.com>
2021-08-02 13:40:45 -0400

    react-modal
    There are three modals in the app, two of which use react-modal and one
    which uses material-ui's modal. Ideally I think material-ui's would be
    used in all three, but for the moment to keep things looking the same
    with CloseModal and modal titles the Trash component has been converted
    to use react-modal.

--- src/client/components/Trash.js
+++ src/client/components/Trash.js
@@ -2,22 +2,31 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import style from './Trash.css'
 
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
+import Modal from 'react-modal'
+import CloseModal from './Insights/CloseModal'
 import Button from '@material-ui/core/Button'
 
-const Trash = ({id, deleteSearch}) => {
+const modalStyle = {
+  content: {
+    padding: 0,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: 300,
+    height: 200,
+  }
+}
 
-  const [open, setOpen] = React.useState(false);
+const Trash = ({id, title, deleteSearch}) => {
+
+  const [open, setOpen] = React.useState(false)
 
   const handleOpen = () => {
     setOpen(true);
-  };
+  }
 
   const handleClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   return (
     <>
@@ -25,33 +34,32 @@ const Trash = ({id, deleteSearch}) => {
         <ion-icon title="Delete Search!" name="trash"></ion-icon>
       </span>
       <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
+        style={modalStyle}
         className={style.Modal}
-        open={open}
+        isOpen={open}
         onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
+        centered
       >
-      <Fade in={open}>
-        <div className={style.Paper}>
-          <h2 id="transition-modal-title">Delete Search?</h2>
-          <p id="transition-modal-description">This action cannot be undone.</p>
-          <Button variant="contained" onClick={() => deleteSearch({id})}>
-            Delete Search
-          </Button>
+        <CloseModal title="Delete Search" close={handleClose} style={{width: 300}} />
+        <div className={style.DeleteSearch}>
+          <p>
+            Would you like to delete your search <span className={style.Title}>{title}</span> ? 
+            Be careful, this cannot be undone!
+          </p>
+          <div className={style.DeleteButton}>
+            <Button variant="contained" onClick={() => deleteSearch({id})}>
+              Delete Search
+            </Button>
+          </div>
         </div>
-      </Fade>
-    </Modal>
-  </>
+      </Modal>
+    </>
   )
 }
 
 Trash.propTypes = {
   id: PropTypes.number,
+  title: PropTypes.string,
   deleteSearch: PropTypes.func
 }
 
