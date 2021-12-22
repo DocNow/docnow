e85bb7b9286f14d40d3544f6ad2a6a0c242074e8
Ed Summers <ehs@pobox.com>
2021-12-22 12:41:03 -0500

    New tweet index
    Add an index on table tweet for (search_id, screen_name) which should
    speed up lookups to see if a user is in a large collection. This index
    can also be used for looking up tweets by search_id alone which means
    that the tweet (search_id) index can be removed.

---
+++ src/server/migrations/20211222123001.tweetscreenname.js
@@ -0,0 +1,14 @@
+exports.up = async knex => {
+  return knex.schema.alterTable('tweet', table => {
+    table.index(['searchId', 'screenName'])
+    // this index is no longer needed with the new one above
+    table.dropIndex(['searchId'])
+  })
+}
+
+exports.down = async knex => {
+  return knex.schema.alterTable('tweet', table => {
+    table.dropIndex(['searchId', 'screenName'])
+    table.addIndex(['searchId'])
+  })
+}
