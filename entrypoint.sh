d88d5d80ef505fb27f92bf1136136cd556a09c0a
Ed Summers <ehs@pobox.com>
2022-01-23 10:18:58 -0500

    Simplify running tests
    The test suite runs automatically via a GitHub Action. It has been possible to run the test suite manually in a development environment but the instructions previously required you to connect to the Postgres Docker container, manually create a test database, and then connect to the web app, edit a file and run the tests.
    
    Now instead you simply copy a file:
    
    ```
    cp .env.test-sample .env.test
    ```
    
    Replace the `CHANGE_ME` values in .env.test to the respective Twitter API credentials.
    
    And then run the tests.
    
    ```
    npm run test
    ```
    
    The tests are run using `docker-compose run` and the test database is
    created automatically. The instructions in the README.md have been
    updated appropriately.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,13 @@ if [ $NODE_ENV = "development" ]; then
   npm run migrate
   npm run start:dev
 elif [ $NODE_ENV = "test" ]; then
+  if [ -f .env.test ]; then
+      set -o allexport
+      source .env.test
+  fi
+  rm test.log
   npm run migrate
-  npm run test
+  npm run test:cmd
 else
   npm run start
 fi
